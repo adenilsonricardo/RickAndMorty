80ab7aeb561f62b0f54eabf822c5ae7950ebff06
Adenilson <adenilsonricardo.bernardes@gmail.com>
2023-04-14 15:23:25 -0300

    criado a camada de data

---
+++ app/src/main/java/com/example/rickandmorty/data/repository/RepositoryRickAndMortyApi.kt
@@ -0,0 +1,8 @@
+package com.example.rickandmorty.data.repository
+
+import com.example.rickandmorty.data.model.ListCharactersModel
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryRickAndMortyApi {
+    fun listCharacters(): Flow<ListCharactersModel>
+}
---
+++ app/src/main/java/com/example/rickandmorty/data/repository/RepositoryRickAndMortyApiImpl.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmorty.data.repository
+
+import com.example.rickandmorty.data.api.RickAndMortyApi
+import com.example.rickandmorty.data.model.ListCharactersModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class RepositoryRickAndMortyApiImpl(
+    private val serviceApi: RickAndMortyApi
+): RepositoryRickAndMortyApi {
+    override fun listCharacters(): Flow<ListCharactersModel> {
+        return flow{
+            emit(serviceApi.listCharacters())
+        }
+    }
+}
